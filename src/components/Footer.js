a6c5986f4aa55c3844e1e497f6e378f299801296
MacBook <macbook@192.168.0.7>
2023-08-14 10:58:55 +0700

    migrasi template ke component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer () {
+    return (
+        <footer className="flex text-center px-4 py-8 justify-center">
+        <p className="text-sm">
+          Copyright 2023 • All Rights Reserved RezSpace by A N Z L
+        </p>
+      </footer>
+    )
+}
+
+export default Footer;
